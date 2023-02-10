b4868c589d4098c8a4e20b4d139451bfbd815a4c
Andrij <andrij.pavlovich@gmail.com>
2023-02-10 20:46:44 +0200

    Create homework folder

---
+++ HW01/pf_01_01.py
@@ -0,0 +1,9 @@
+# Python fundamentals 01
+# Practical task1
+name = input('What is your name? ')
+age = input('How old are you? ')
+location = input('Where do you live? ')
+
+print(f'Hello, {name}')
+print(f'Your age is {age}')
+print(f'You live in {location}')
---
+++ HW01/pf_01_02.py
@@ -0,0 +1,13 @@
+# Python fundamentals 01
+# Practical task1
+
+a = int(input('Enter first integer number "a": '))
+b = int(input('Enter second integer number "b": '))
+
+print('a + b =', a + b)
+print('a - b =', a - b)
+print('a * b =', a * b)
+print('a / b =', a / b)
+print('a ** b =', a ** b)
+print('a // b =', a // b)
+print('a % b =', a % b)
