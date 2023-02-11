8ae00f6d4eda8063e94e396cac1e4dc2ebfc24e2
dyeskive <sergiy.goryainov@gmail.com>
2023-02-11 14:24:55 +0200

    code asking name, age and location

---
+++ Homework/HW01/HW01.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")  # input user's name into var
+print("Hello, " + name + "! Nice to meet you!")  # print var containing user's name input
+
+age = int(input(name + ", how old are you? "))  # input user's age into var
+print("You are " + str(age) + " years old.")  # print var containing user's age input
+
+location = input("Where do you live, " + name + "? ")  # input user's location into var
+print("You live in " + location)
