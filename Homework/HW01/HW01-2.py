86f81d8b458bb4f5e05e1bee0b9d81ccb9bbf6c0
dyeskive <sergiy.goryainov@gmail.com>
2023-02-11 15:29:54 +0200

    code performing basic arithmetic functions for 2 integers

---
+++ Homework/HW01/HW01-2.py
@@ -0,0 +1,19 @@
+a = int(input("Please enter a whole number (A): "))
+b = int(input("Please enter another whole number (B): "))
+
+sum = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+exponent = a ** b
+floor = a // b
+remainder = a % b
+
+
+print("A + B =", sum)
+print("A - B =", difference)
+print("A * B =", product)
+print("A / B =", quotient)
+print("A ** B =", exponent)
+print("A // B =", floor)
+print("A % B =", remainder)
