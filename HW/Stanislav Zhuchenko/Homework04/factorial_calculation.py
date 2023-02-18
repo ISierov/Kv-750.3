0c7a351704baab9a4b8c00c65306091d901bbf9e
Stanislav Zhuchenko <Zhuchenko_Stas@ukr.net>
2023-02-18 15:25:59 +0200

    Add 4th homework

--- HW/Stanislav Zhuchenko/Homework04/factorial_calculation.py
+++ HW/Stanislav Zhuchenko/Homework04/factorial_calculation.py
@@ -1,10 +1,17 @@
+import math
+
 number = int(input('Please input the integer number more or equal 0: \nn = '))
-counter = 0
-res = 1
-while counter != number:
-    res *= number
-    counter += 1
-print(res)
+
+
+def factorial(int: number):
+    res = 1
+    for i in range(1, number+1):
+        res *= i
+    return res
+
+
+print(f'The factorial of number {number} is equal:', factorial(number))
+#print(factorial(number) == math.factorial(number))
 
 
 
