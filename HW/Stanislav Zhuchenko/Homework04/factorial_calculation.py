1ae7a48f7e70e1a404e8ce313481f1af5660f13f
Stanislav Zhuchenko <Zhuchenko_Stas@ukr.net>
2023-02-18 14:45:55 +0200

    Add 4th homework

---
+++ HW/Stanislav Zhuchenko/Homework04/factorial_calculation.py
@@ -0,0 +1,10 @@
+number = int(input('Please input the integer number more or equal 0: \nn = '))
+counter = 0
+res = 1
+while counter != number:
+    res *= number
+    counter += 1
+print(res)
+
+
+
