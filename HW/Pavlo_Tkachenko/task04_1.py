ade8c8965cabd2a7f0861c41f03db805a0273eb6
pythonStudent88 <g.pavel.sport@gmail.com>
2023-02-22 14:44:54 +0200

    pavlo tkachenko - lesson 4

--- HW/Pavlo_Tkachenko/task04_1.py
+++ HW/Pavlo_Tkachenko/task04_1.py
@@ -1,4 +1,5 @@
-num = int(input("Calculate factorial.\nThe number is "))
+# task to calc factorial input number
+num = int(input("Calculate factorial.\n Number is "))
 res = 1
 for i in range(1, num+1, 1):
     res = res*i
