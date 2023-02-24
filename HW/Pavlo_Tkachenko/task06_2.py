87638e4d3207bbfde532e27ccd69434b5e940681
pythonStudent88 <g.pavel.sport@gmail.com>
2023-02-24 17:52:56 +0200

    pavlo tkachenko - lesson 6

---
+++ HW/Pavlo_Tkachenko/task06_2.py
@@ -0,0 +1,10 @@
+
+while True:
+    login = input('Your login name is ')
+
+    if login == 'First':
+        print('   Welcome, mr.'+login)
+        break
+    else:
+        print('Warning! You entered login \''+login+'\' is differ than \'First\'')
+
