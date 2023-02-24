02a3f014b1121c3124950e042b2489c9fbbc0c84
Ivan Sierov <isier@softserveinc.com>
2023-02-24 18:57:43 +0200

    meet5-6

---
+++ Meet5.py
@@ -0,0 +1,22 @@
+import Meet6
+# #Check if the list contains odd numbers (using break)
+# arr = [0, 1, 2, 3, 4, 5, 6, 7]
+#
+# arr.extend([1345])
+# print(arr)
+# arr += [123]
+# print(arr)
+
+# a = [1, 2, [3, 4, 5]]
+# b = a
+# c = a[:][:]
+# c[2] = a[2][:]
+# print(a, id(a))
+# print(b, id(b))
+# print(c, id(c))
+# c[2][1] = 100
+# print(a, id(a))
+# print(b, id(b))
+# print(c, id(c))
+
+Meet6.lin_search(1, [1, 2, 3, 4])
