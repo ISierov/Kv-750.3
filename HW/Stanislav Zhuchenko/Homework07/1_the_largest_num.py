38031f086e6908b5859c62343e5a865c187aaf21
Stanislav Zhuchenko <Zhuchenko_Stas@ukr.net>
2023-02-27 19:20:45 +0200

    Add 3 practical tasks from LMS

---
+++ HW/Stanislav Zhuchenko/Homework07/1_the_largest_num.py
@@ -0,0 +1,11 @@
+def the_largest_number(a: float, b: float):
+    """
+    The function return the largest number of two numbers: a or b
+    :param a: number, type - integer or float
+    :param b: number, type - integer or float
+    :return: the largest number from a and b, type - integer or float
+    """
+    return max(a, b)
+
+
+print(the_largest_number(3.1, 2))
