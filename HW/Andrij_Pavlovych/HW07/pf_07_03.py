38faafec56f03adaed7028242095bd2e3620121c
Andrij <andrij.pavlovich@gmail.com>
2023-03-01 20:25:34 +0200

    Changed homework 07: pf_07_03

--- HW/Andrij_Pavlovych/HW07/pf_07_03.py
+++ HW/Andrij_Pavlovych/HW07/pf_07_03.py
@@ -2,8 +2,11 @@
 # included in a given string
 
 def string_counter(my_str):
-    return {i: list(my_str).count(i) for i in my_str}
+    return {i: my_str.count(i) for i in my_str}
 
 my_str = input('Enter a string: ')
 
 print(string_counter(my_str))
+
+
+
