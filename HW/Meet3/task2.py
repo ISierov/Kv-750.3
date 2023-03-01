2a37f6dad46205a712371ee774543fcbe2368055
Ivan Sierov <isier@softserveinc.com>
2023-03-01 17:02:23 +0200

    del file + meet7 push

--- HW/Meet3/task2.py
+++
@@ -1,6 +0,0 @@
-num = 5376
-reversed_num = int(str(num)[::-1])
-sorted_num = int("".join(sorted(str(num))))
-
-print(reversed_num)
-print(sorted_num)
--- HW/Meet3/task3.py
+++
@@ -1,6 +0,0 @@
-x = 42
-y = 23
-print(f"x = {x}\t" f"y = {y}")
-
-x, y = y, x
-print(f"x = {x}\t" f"y = {y}")
