e3a945c46497ccb2891dca8a25110559326c9d7a
Oleksandr Liashenko <o.i.liashenko@gmail.com>
2023-03-07 00:10:01 +0200

    Create HW07_cw_06.py

---
+++ HW/Oleksandr Liashenko/HW07_Codewars/HW07_cw_06.py
@@ -0,0 +1,11 @@
+"""Reverse List Order
+
+In this kata you will create a function that takes in a list and returns a list with the reverse order."""
+
+
+def reverse_list(l):
+    l.reverse()
+    return l
+
+
+print(reverse_list([2, 4, 7, 8]))
