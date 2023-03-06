94a655719456e56257344c0c6721773eec0d7c1f
Oleksandr Liashenko <o.i.liashenko@gmail.com>
2023-03-07 00:09:56 +0200

    Create HW07_cw_04.py

---
+++ HW/Oleksandr Liashenko/HW07_Codewars/HW07_cw_04.py
@@ -0,0 +1,16 @@
+"""Convert a Number to a String!
+
+We need a function that can transform a number (integer) into a string.
+What ways of achieving this do you know?"""
+
+
+def number_to_string(num):
+    return str(num)
+
+
+number = 5647
+string = number_to_string(number)
+
+print(number_to_string(number))
+print(type(number))
+print(type(string))
